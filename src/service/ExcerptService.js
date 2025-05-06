48a1d2b2c3ba0135f50b0a7403c23b76c713bb9a
selmangummussoy <gumussoyhsg@gmail.com>
2025-05-06 19:38:49 +0300

    profil sayfasında benim alıntılarımı görüntüle sayfası

--- src/service/ExcerptService.js
+++ src/service/ExcerptService.js
@@ -63,5 +63,22 @@ export const ExcerptService = {
         if (!response.ok) {
             throw new Error('Failed to delete excerpt');
         }
+    },
+
+    async getExcerptsByUserId(userId) {
+        const token = localStorage.getItem("jwtToken");
+        const response = await fetch(`http://localhost:8080/excerpts/${userId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch excerpts by userId");
+        }
+
+        return await response.json();
     }
 };
